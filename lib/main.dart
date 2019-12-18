7273decdc73451e538cf6e5184e0feeb8284b798
hunter2009pf <hunter2009pf@outlook.com>
2019-12-18 22:34:53 +0800

    first day

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+import 'login.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'play together',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text("家教 1 vs 1"),centerTitle: true,),
+        body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                colorFilter: ColorFilter.mode(Color.fromARGB(50, 100, 100, 100), BlendMode.darken),
+                image: AssetImage("assets/teacher.jpg"),
+                fit: BoxFit.cover
+              )
+            ),
+            child: Login()
+        ),
+      ),
+      routes: {
+        "homePage": (context)=>HomePage()
+      },
+    );
+  }
+}
